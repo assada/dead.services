a98c99bbfba7cd0a8492a3f1c0dedea8a0737b75
assada <assada.ua@gmail.com>
2021-02-17 16:19:58 +0200

    Prom fix

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -4,7 +4,7 @@ docker run \
     -d \
     --restart=always \
     -p 9090:9090 \
-    -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v './prometheus.yml:/etc/prometheus/prometheus.yml' \
     -v prometheus:/prometheus \
     --name=prometheus \
     prom/prometheus
