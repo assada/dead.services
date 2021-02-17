6c81f95c7e00b49d73005ebbcbaf6538499493eb
assada <assada.ua@gmail.com>
2021-02-17 16:34:10 +0200

    Filebeat fix

--- filebeat/filebeat.sh
+++ filebeat/filebeat.sh
@@ -4,8 +4,11 @@ docker run -d \
   --restart=always \
   --name=filebeat \
   --user=root \
+  --network="dead-services" \
   --volume="$(pwd)/filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
   --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
   --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
-  docker.elastic.co/beats/filebeat:7.5.1 filebeat -e -strict.perms=false \
-  -E output.elasticsearch.hosts=["dead.services:9200"]
+  docker.elastic.co/beats/filebeat:7.6.2 filebeat -e -strict.perms=false \
+  -E output.elasticsearch.hosts=["elasticsearch:9200"] \
+  -E output.elasticsearch.username="elastic" \
+  -E output.elasticsearch.password="secret"
