f65c84a74f36435241e93b0290e53b13058660ee
assada <assada.ua@gmail.com>
2021-02-17 16:43:26 +0200

    Telegraf fix

--- telegraf/telegraf.sh
+++ telegraf/telegraf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name=telegraf --net=dead-services -p 8125:8125/udp -p 9273:9273 --restart=always -v "$(pwd)./telegraf.conf:/etc/telegraf/telegraf.conf:ro" telegraf
+docker run -d --name=telegraf --net=dead-services -p 8125:8125/udp -p 9273:9273 --restart=always -v "$(pwd)/telegraf.conf:/etc/telegraf/telegraf.conf:ro" telegraf
